5ff5264f3a482138e9081ec019473bc4b00143f4
sumandhami <sumandhami86@gmail.com>
2024-11-25 11:09:38 +0545

    dart data types and variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text("Hello World")));
+  String name = "Suman";
+  int age = 21;
+  int sum = add(10, 20);
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+              "Hello $name, you are $age years old and result of sum is $sum"),
+        ),
+      ),
+    ),
+  );
+}
+
+int add(num1, num2) {
+  return num1 + num2;
 }
