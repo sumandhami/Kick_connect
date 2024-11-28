80d7f7cd00cab6bed513ce219446f1ba14951d0f
sumandhami <sumandhami86@gmail.com>
2024-11-29 01:58:34 +0545

    Creating Portfolio App

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,133 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  String name = "Suman";
-  int age = 21;
-  int sum = add(10, 20);
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text(
-              "Hello $name, you are $age years old and result of sum is $sum"),
-        ),
-      ),
-    ),
-  );
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
 }
 
-int add(num1, num2) {
-  return num1 + num2;
+class Home extends StatelessWidget {
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(top: 100.0, left: 20),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 40,
+                ),
+                SizedBox(
+                  width: 50,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Suman Dhami",
+                      style: TextStyle(fontSize: 30.0),
+                    ),
+                    Text(
+                      "Designation",
+                      style: TextStyle(fontSize: 15),
+                    )
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 30),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.school,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "School Name",
+                        style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.computer_rounded,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "School Name",
+                        style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.location_pin,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "School Name",
+                        style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "School Name",
+                        style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "School Name",
+                        style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Text("About Me"),
+            Text("Created By Suman"),
+          ],
+        ),
+      ),
+    );
+  }
 }
