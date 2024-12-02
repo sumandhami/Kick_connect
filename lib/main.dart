ee2721769f4182631ed5f934853c91b3c184af61
sumandhami <sumandhami86@gmail.com>
2024-12-02 20:31:43 +0545

    Completed the Portfolio App

--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,186 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Home(),
-  ));
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    ));
+  });
 }
 
 class Home extends StatelessWidget {
-  const Home({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(top: 100.0, left: 20),
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 40,
-                ),
-                SizedBox(
-                  width: 50,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "Suman Dhami",
-                      style: TextStyle(fontSize: 30.0),
-                    ),
-                    Text(
-                      "Designation",
-                      style: TextStyle(fontSize: 15),
-                    )
-                  ],
-                )
-              ],
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 30),
-              child: Column(
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/dark.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(top: 100.0, left: 20),
+          child: Column(
+            children: <Widget>[
+              Row(
                 children: <Widget>[
-                  Row(
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundImage: AssetImage("images/user.jpg"),
+                  ),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Icon(
-                        Icons.school,
-                        size: 40,
-                      ),
-                      SizedBox(
-                        width: 20,
+                      Text(
+                        "Suman Dhami",
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          color: Colors.white,
+                        ),
                       ),
                       Text(
-                        "School Name",
-                        style: TextStyle(fontSize: 20),
+                        "Designation",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                        ),
                       )
                     ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.computer_rounded,
-                        size: 40,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text(
-                        "School Name",
-                        style: TextStyle(fontSize: 20),
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.location_pin,
-                        size: 40,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text(
-                        "School Name",
-                        style: TextStyle(fontSize: 20),
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.email,
-                        size: 40,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text(
-                        "School Name",
-                        style: TextStyle(fontSize: 20),
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.phone,
-                        size: 40,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text(
-                        "School Name",
-                        style: TextStyle(fontSize: 20),
-                      )
-                    ],
-                  ),
+                  )
                 ],
               ),
-            ),
-            Text("About Me"),
-            Text("Created By Suman"),
-          ],
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 30),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.school,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "HCOE",
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.computer_rounded,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "Engineering",
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.location_pin,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "Kathmandu",
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.email,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "suman@gmail.com",
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.phone,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "9800000000",
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  "About Me This is the portfolio about me and i have really done a great job for my portfolio",
+                  style: TextStyle(
+                      fontSize: 22, color: Colors.white, fontFamily: 'Roboto'),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Created By Suman",
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
         ),
       ),
     );
